5f063b9ef930356f7f5ba8b1d21f6facb4441a66
Ygor <99401550+FatalRestart@users.noreply.github.com>
2023-09-05 10:33:42 -0300

    att
    botando 3 meses de aula

--- my_site/settings.py
+++ my_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '3307',
     }
 }
 
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        "latest_question_list": latest_question_list
+        }
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta de número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     return HttpResponse(f"Resultados da pergunta de número {question_id}")
 
 def vote(request, question_id):
     return HttpResponse(f"Você vai votar na pergunta de número {question_id}")
+
+from django.views.generic import CreateView, ListView, DetailView
+from django.urls import reverse_lazy
+
+class QuestionCreateView(CreateView):
+    model = Question 
+    fields = ('question_text',)
+    success_url = reverse_lazy('index')
+    template_name = 'polls/question_form.html'
+
+class QuestionListView(ListView):
+    model = Question
+    context_object_name = 'questions'
+
+class QuestionDetailView(DetailView):
+    model = Question
+    context_object_name = 'question'
